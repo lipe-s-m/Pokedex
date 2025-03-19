602119f244e2f38606f1e6640b8368b3c8968283
lipe-s-m <felipe.s.m1609@gmail.com>
2025-03-19 03:03:24 -0300

    :tada: Deploy Project

--- src/app/models/pokemonDataList.ts
+++ src/app/models/pokemonDataList.ts
@@ -1,13 +1,11 @@
-import { PokemonData } from './PokemonData';
-
 export type PokemonDataList = {
-  region: string;
-  quantidadePokemons?: number;
-  pokemon_entries: {
-    entry_number: number;
-    pokemon_species: {
-      name: string;
-      url: string;
-    };
-  }[];
+    region: string;
+    quantidadePokemons?: number;
+    pokemon_entries: {
+        entry_number: number;
+        pokemon_species: {
+            name: string;
+            url: string;
+        };
+    }[];
 };
